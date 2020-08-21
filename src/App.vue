d91f1711ab1fdab0d30df299a43b6074e22f378d
SomyodMookdasawan <68525422+SomyodMookdasawan@users.noreply.github.com>
2020-08-21 20:46:59 +0700

    Start Use Git

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app" :style="{'background-image':'url(https://407261-1281864-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/10/infant-pink-backdrop-416x395.jpg)'}">
+    <HelloWorld msg="LIKE-LOVE-UNLIKE-RESET" /><hr><br>
+    <div class="row">
+      <div class="col-12">
+        <img alt="Vue logo" src="./assets/1.jpg" width="490" /><br><br><hr>
+        <h1>{{total}}</h1>
+      </div>
+      <div class="col-12">
+        <CsButton @cc="sum" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue"
+import CsButton from "./components/CsButton.vue"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CsButton,
+  },
+  data: function(){
+    return {
+      total: 0,
+    };
+  },
+
+  methods: {
+    sum(counterValue) {
+      this.total = counterValue;
+    },
+  },
+};
 </script>
 
 <style>
